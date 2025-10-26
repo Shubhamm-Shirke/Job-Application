fac55b2cc2c99f166d50504a004386fe782d1b62
Shubham <shirkeshubham986@gmail.com>
2025-10-26 15:54:10 +0530

    1

---
+++ src/test/java/com/shubham/jobApp/JobAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shubham.jobApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
